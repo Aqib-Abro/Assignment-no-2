d4322e58487e86cba2a1de1541d0b89321f46a8f
Aqib-Abro <abroaqib111@gmail.com>
2024-10-27 21:00:07 +0500

    Add files via upload

---
+++ TemperatureConverter1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main (){
+float celcius=5,fahrenheit;
+fahrenheit = (celcius * 9/5) + 32;
+printf("%f celcius = %f fahrenheit",celcius, fahrenheit);
+getch(); 
+}
